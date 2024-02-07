c1756b684c81a0bcf6fea3065a4031af5d49a253
Rishabh Maindola <143874827+rishabhmaindola@users.noreply.github.com>
2024-02-07 21:24:06 +0530

    Update App.jsx

--- chatapp/src/App.jsx
+++ chatapp/src/App.jsx
@@ -18,7 +18,7 @@ import Music from "./Components/Music";
 import { FaMusic } from "react-icons/fa";
 import SoundPanel from "./Components/SoundPanel";
 
-const socket = io("https://rizzchat-server.vercel.app", {
+const socket = io("https://rizzchat-server-rishabhmaindolas-projects.vercel.app/", {
   transports: ["websocket"],
 });
 
